308480d7ac7b9c428482fa32bbb5a9e21c59c694
Erik C. Thauvin <erik@thauvin.net>
2015-11-11 01:36:07 -0800

    Updated to commons-net 1.4.0.

--- src/net/thauvin/lifeblogger/ReleaseInfo.java
+++ src/net/thauvin/lifeblogger/ReleaseInfo.java
@@ -1,5 +1,5 @@
 /* Created by JReleaseInfo AntTask from Open Source Competence Group */
-/* Creation date Thu Apr 14 03:27:19 PDT 2005 */
+/* Creation date Wed Nov 11 01:32:04 PST 2015 */
 package net.thauvin.lifeblogger;
 
 import java.util.Date;
@@ -12,21 +12,24 @@ import java.util.Date;
 public class ReleaseInfo {
 
 
-   /** buildDate (set during build process to 1113474439925L). */
-   private static Date buildDate = new Date(1113474439925L);
+   /** buildDate (set during build process to 1447234324519L). */
+   private static Date buildDate = new Date(1447234324519L);
 
    /**
-    * Get buildDate (set during build process to Thu Apr 14 03:27:19 PDT 2005).
+    * Get buildDate (set during build process to Wed Nov 11 01:32:04 PST 2015).
     * @return Date buildDate
     */
    public static final Date getBuildDate() { return buildDate; }
 
 
+   /** project (set during build process to "LifeBlogger"). */
+   private static String project = new String("LifeBlogger");
+
    /**
-    * Get buildNumber (set during build process to 1).
-    * @return int buildNumber
+    * Get project (set during build process to "LifeBlogger").
+    * @return String project
     */
-   public static final int getBuildNumber() { return 1; }
+   public static final String getProject() { return project; }
 
 
    /** version (set during build process to "0.2"). */
@@ -39,13 +42,10 @@ public class ReleaseInfo {
    public static final String getVersion() { return version; }
 
 
-   /** project (set during build process to "LifeBlogger"). */
-   private static String project = new String("LifeBlogger");
-
    /**
-    * Get project (set during build process to "LifeBlogger").
-    * @return String project
+    * Get buildNumber (set during build process to 2).
+    * @return int buildNumber
     */
-   public static final String getProject() { return project; }
+   public static final int getBuildNumber() { return 2; }
 
 }
